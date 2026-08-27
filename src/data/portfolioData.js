18dd53c0ee44ac9a4dccac338ab02030337e0185
Addissa Samson <samsonaddissa247@gmail.com>
2026-08-27 10:40:30 +0200

    add live link and repo link

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -77,8 +77,8 @@ export const projects = [
     description: "A platform for my self to do my work and learn new things.",
     image: "/images/",
     technologies: ["UX/UI Design", "HTML", "CSS", "JavaScript"],
-    demo: "",
-    github: "",
+    demo: "https://myuniverse-eight.vercel.app/",
+    github: "https://github.com/samsonaddissa247-creator/myuniverse",
     
   },
   {
